dc285fadc63b5c8ff52036d0bb0eb0bc1f8a8069
Michael Feinberg <mfeinberg32999@gmail.com>
2016-10-17 11:44:03 -0400

    Wrote insert_song and Print_list

--- list.c
+++ list.c
@@ -16,20 +16,49 @@ song_node * insert_front(song_node *front, char newName[], char newArtist[]){
   return newFront;
 }
 
-void insert_song(song_node *front, char newName[], char newArtist[]){
-  song_node * newSong =  (song_node *)malloc(sizeof(song_node));
-
-  song_node * p = front;
-  if(!*p){
-    insertFront(front,newName,newArtist);
-  }else if(!*(p->next)){
-    p = p->next;
-    front->next = insertFront(p,newName,newArtist);
-  }else{
-    while(!*((p->next)->next)){
-      if(strncmp(newArtist,p->artist,256)<0){
-	
-      }
-    }
+song_node * insert_song(song_node *front, char newName[], char newArtist[]){
+  //If list is null or song is alphabetically before first element
+  if(!*front || strncmp(newArtist,front->artist,256)<0){
+    return insert_front(front,newName,newArtist);
   }
+  
+  song_node * prev = front;
+  song_node * curr = front->next;
+  
+  char *currArtist = curr->artist;
+  //loop through list until null or until the song is alphabetically first
+  while(*curr && strncmp(newArtist,currArtist,256)>0){
+    prev = curr;
+    curr = curr->next; 
+  }
+  //insert at stopped position
+  song_node * endList = insert_front(curr,newName,newArtist);
+  
+  //link entire list and return front
+  prev->next = endList;
+  return front;
+  
+}
+
+void print_list(song_node * front){
+  if(front==NULL){
+    printf("There are no songs on this list\n");
+  }else{
+    while(front->next != NULL){
+      printf("Name: %s\n", front->name);
+      printf("Artist: %s\n\n", front->artist);
+      front = front->next;
+    }
+    
+  }
+}
+
+int main(){
+  
+
+
+
+
+
+  return 0;
 }
